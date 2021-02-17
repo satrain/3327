9639a612604b888ddbcf3327fbbd96055a95adc0
satrain <mladen.trickovic5@gmail.com>
2021-02-17 23:36:49 +0100

    about page creation

--- about.php
+++ about.php
@@ -3,7 +3,24 @@
     get_header();
 ?>
 <main>
-    <div class="about-us">
+    <div class="about-us-hero">
+        <div class="about-us-hero-content">
+            <div class="about-us-hero-copy">
+                <h1>about us.</h1>
+                <h2>We're experimenting with Web 3.0 technologies.</h2>
+                <br>
+                <a href="#" class="btn" id="about-us-btn">work with us</a>
+            </div>
+            <div class="about-us-hero-mockup">
+                <div class="circle right-circle about-us-hero-circle">
+                    <div class="person-one-hand"></div>
+                    <div class="person-two-hand"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="about-us-cta">
         <div class="about-join-us">
             <h1>Join this forward-thinking, tall and <br>handsome crew!</h1>
             <p>The worst thing that can happen is we mess everything up and <br>epically fail.</p>
