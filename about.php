55ae66319aac111f9bfd9f79693dd508468ef602
satrain <mladen.trickovic5@gmail.com>
2021-02-18 01:39:42 +0100

    init commit

--- about.php
+++ about.php
@@ -3,6 +3,7 @@
     get_header();
 ?>
 <main>
+    <!-- About us hero -->
     <div class="about-us-hero">
         <div class="about-us-hero-content">
             <div class="about-us-hero-copy">
@@ -11,7 +12,7 @@
                 <br>
                 <a href="#" class="btn" id="about-us-btn">work with us</a>
             </div>
-            <div class="about-us-hero-mockup">
+            <div class="about-us-hero-mock">
                 <div class="circle right-circle about-us-hero-circle">
                     <div class="person-one-hand"></div>
                     <div class="person-two-hand"></div>
@@ -20,11 +21,50 @@
         </div>
     </div>
 
+    <!-- What are we looking for | wawlf -->
+    <div class="what-are-we-looking-for">
+        <div class="wawlf-content">
+            <div class="wawlf-copy">
+                <h2>What are we looking for</h2>
+                <p>- Solutions to the problems we face as a community gathered around a new Web.</p>
+                <p>- Better understanding of the technology, its capabilities, and ways we can use it to make progress.</p>
+                <p>- New ideas that can propel Web 3.0 forward.</p>
+            </div>
+            <div class="wawlf-mock">
+                <div class="circle right-circle wawlf-circle">
+                    <div class="person-with-the-flag"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Join this forward-thinking, tall and handsome crew!
+         About us CTA -->
     <div class="about-us-cta">
         <div class="about-join-us">
             <h1>Join this forward-thinking, tall and <br>handsome crew!</h1>
             <p>The worst thing that can happen is we mess everything up and <br>epically fail.</p>
             <button class="btn about-join-us-btn">join us</button>
+        </div>
+    </div>
+
+    <!-- We believe in -->
+    <div class="we-believe-in">
+        <div class="we-believe-in-content">
+            <div class="we-believe-in-mock">
+                <div class="circle left-circle we-believe-in-circle">
+                    <div class="person-who-is-walking"></div>
+                </div>
+            </div>
+            <div class="we-believe-in-copy">
+                <h2>We believe in</h2>
+                <h3>Collaboration adn teamwork.</h3>
+                <p>They greatly contribute to learning and growth.</p>
+                <h3>Decentralization.</h3>
+                <p>We see blockchain as an enabler of decentralization ideas.</p>
+                <h3>Open standards and knowledge sharing.</h3>
+                <p>We are enthusiastic about learning and are looking to foster a life-long learning process.</p>
+            </div>
         </div>
     </div>
 
