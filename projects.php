6dd195de5794f92729d0879f6df6a771867ab8b0
satrain <mladen.trickovic5@gmail.com>
2021-02-23 18:52:29 +0100

    projects figures added

--- projects.php
+++ projects.php
@@ -17,144 +17,203 @@
             </div>
         </div>
     </div>
+    <div class="projects-wrapper">
+        <!-- Scriptarnica -->
+        <div class="slide scriptarnica-slide projects-container">
+            <div class="figure-mock scriptarnica-mock">
+                <div class="scriptarnica-image1"></div>
+                <div class="scriptarnica-image2"></div>
+                <div class="scriptarnica-image3"></div>
+            </div>
+            <div>
+                <h3>Scriptarnica</h3>
+                <p>Scriptarnica is a decentralized platform for reading, writing, publishing, selling, and buying content. The platform was developed with an aim to enable fair content sharing in the new era of the Internet by establishing close contact between an author, his audience, and all the people in-between.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-scriptcarnica-btn">try it</a>
+            </div>
+        </div>
 
-    <!-- Scriptarnica -->
-    <div class="slide scriptarnica-slide">
-        <div>
-            <h3>Scriptarnica</h3>
-            <p>Scriptarnica is a decentralized platform for reading, writing, publishing, selling, and buying content. The platform was developed with an aim to enable fair content sharing in the new era of the Internet by establishing close contact between an author, his audience, and all the people in-between.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-scriptcarnica-btn">try it</a>
+        <!-- Bizzllet -->
+        <div class="slide bizzllet-slide projects-container">
+            <div class="figure-mock bizzllet-mock">
+                <div class="bizzllet-image1"></div>
+                <div class="bizzllet-image2"></div>
+            </div>
+            <div>
+                <h3>Bizzllet</h3>
+                <p>Bizzllet is imagined as a platform for business users that will bring new opportunities to seamlessly store, manage, integrate and distribute a company's cryptocurrency portfolio across multiple wallets with different integrations to meet the business needs. All that packed into a user-friendly interface for everyone coming from the current Web2 world to be able to easily enter the new era of payments and doing business on the blockchain.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-bizzllet-btn">check prototype</a>
+            </div>
+        </div>
+
+        <!-- Substrate Blockchain Starter Kit -->
+        <div class="slide sbsk-slide projects-container">
+            <div class="figure-mock sbsk-mock">
+                <div class="sbsk-image1"></div>
+                <div class="sbsk-image2"></div>
+            </div>
+            <div>
+                <h3>Substrate Blockchain <br>Starter Kit</h3>
+                <p>It is a tool that brings the simplicity of creating a blockchain to another level! By drag and dropping pallets to a codebase, anyone can have a customized blockchain deployed in less than a minute.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-sbsk-btn">build your blockchain</a>
+            </div>
+        </div>
+
+        <!-- Stealth DEX PoC L2  -->
+        <div class="slide sdpl-slide projects-container">
+            <div class="figure-mock sdpl-mock">
+                <div class="sdpl-image1"></div>
+                <div class="sdpl-image2"></div>
+            </div>
+            <div>
+                <h3>Stealth DEX PoC L2</h3>
+                <p>Stealth DEX is a L2 orderbook based, spot trading exchange protocol built on top of Ethereum and Loopring.DEX uses zk-proofs to validate all trades on the Ethereum mainnet, thus effectively deriving security from it. It uses a "Volition" approach, i.e. hybrid on-chain/off-chain data storage. The flexibility of the "Volition" approach still allows users to maintain the highest level of security by allowing them to store the data on the Ethereum mainnet (just like they do with "Rollups") or locally, i.e. using their own storage.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-sdpl-btn">work with us</a>
+            </div>
+        </div>
+
+        <!-- Whollet -->
+        <div class="slide whollet-slide projects-container">
+            <div class="figure-mock whollet-mock">
+                <div class="whollet-image1"></div>
+                <div class="whollet-image2"></div>
+                <div class="whollet-image3"></div>
+            </div>
+            <div>
+                <h3>Whollet</h3>
+                <p>Whollet is a comprehensive crypto wallet UI kit we created by gathering the best practices (some of which we developed) for everyone to use and customize freely. You may recognize many UI elements, as many projects such as Coinbase, Argent, Celsius, and others are using similar information architecture and interaction patterns.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-whollet-btn">download</a>
+            </div>
+        </div>
+
+        <!-- Smart Contract Vulnerabilities Database -->
+        <div class="slide scvd-slide projects-container">
+            <div class="figure-mock scvd-mock">
+                <div class="scvd-image1"></div>
+                <div class="scvd-image2"></div>
+            </div>
+            <div>
+                <h3>Smart Contract <br>Vulnerabilities <br>Database</h3>
+                <p>Whollet is a comprehensive crypto wallet UI kit we created by gathering the best practices (some of which we developed) for everyone to use and customize freely. You may recognize many UI elements, as many projects such as Coinbase, Argent, Celsius, and others are using similar information architecture and interaction patterns.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-scvd-btn">download</a>
+            </div>
+        </div>
+
+        <!-- Crypto Gift Cards -->
+        <div class="slide cgc-slide projects-container">
+            <div class="figure-mock cgc-mock">
+                <div class="cgc-image1"></div>
+                <div class="cgc-image2"></div>
+            </div>
+            <div>
+                <h3>Crypto Gift Cards</h3>
+                <p>A tool for the creation of gift cards topped up with cryptocurrency. Simple way to introduce someone to cryptocurrencies.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-cgc-btn">create a card</a>
+            </div>
+        </div>
+
+        <!-- Crypto Trophies -->
+        <div class="slide crypto-trophies-slide projects-container">
+            <div class="figure-mock crypto-trophies-mock">
+                <div class="crypto-trophies-image1"></div>
+                <div class="crypto-trophies-image2"></div>
+            </div>
+            <div>
+                <h3>Crypto Trophies</h3>
+                <p>Crypto trophies is an employee recognition tool offering unique badges (NFTs) for organization or company promotion/employee growth + scoreboard within the organization (passed trial, got promotion, anniversary, peers recognition, etc.)</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-crypto-trophies-btn">give some recognition</a>
+            </div>
+        </div>
+
+        <!-- Liquid Crystal  -->
+        <div class="slide liquid-crystal-slide projects-container">
+            <div class="figure-mock liquid-crystal-mock">
+                <div class="liquid-crystal-image1"></div>
+                <div class="liquid-crystal-image2"></div>
+            </div>
+            <div>
+                <h3>Liquid Crystal</h3>
+                <p>Liquid Crystal is a blockchain created with an idea to avoid the current bottleneck where all transactions are targeting shared ledger on the blockchain, by enabling communication on a smaller scale. Imagine different blockchains composed into a tree hierarchy working and communicating together as one. This model is enabling faster transactions together with keeping gas fees very low. Too good to be true?</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-liquid-crystal-btn">join the crew</a>
+            </div>
+        </div>
+
+        <!-- Our network -->
+        <div class="slide our-network-slide projects-container">
+            <div class="figure-mock our-network-mock">
+                <div class="our-network-image1"></div>
+                <div class="our-network-image2"></div>
+            </div>
+            <div>
+                <h3>Our network</h3>
+                <p>A community-driven platform for cryptanalysis. With Our Network, analysts from any discipline can contribute concise insights to the broader community while gaining valuable feedback in the form of voting and commentary. And end-users can receive a curated and personalized crypto analytics experience.Anyone should be able to take the data sources available today and derive unique, concise insights for users, investors, and teams to learn from; effectively creating a leaderboard with the best data-driven content curated by the community members.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-our-network-btn">join now</a>
+            </div>
+        </div>
+
+        <!-- Decentralized Business Model Canvas -->
+        <div class="slide dbmc-slide projects-container">
+            <div class="figure-mock dbmc-mock">
+                <div class="dbmc-image1"></div>
+                <div class="dbmc-image2"></div>
+            </div>
+            <div>
+                <h3>Decentralized Business <br>Model Canvas</h3>
+                <p>New approach to the classic business model canvas, adapted for validation of decentralized business models.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-dbmc-btn">join the crew</a>
+            </div>
+        </div>
+
+        <!-- Token Modelling Canvas -->
+        <div class="slide tmc-slide projects-container">
+            <div class="figure-mock tmc-mock">
+                <div class="tmc-image1"></div>
+                <div class="tmc-image2"></div>
+            </div>
+            <div>
+                <h3>Token Modelling <br>Canvas</h3>
+                <p>New take on a framework for designing balanced and sustainable tokenomics models. It's been created as an additional tool to be used with DBMC, but it can be used entirely separately.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-tmc-btn">download</a>
+            </div>
+        </div>
+
+        <!-- Blockchainz Design -->
+        <div class="slide blockchainz-design-slide projects-container">
+            <div class="figure-mock blockchainz-design-mock">
+                <div class="blockchainz-design-image1"></div>
+                <div class="blockchainz-design-image2"></div>
+            </div>
+            <div>
+                <h3>Blockchainz Design</h3>
+                <p>A place for designers in the blockchain world, showcasing the best projects, articles, resources and ideas.</p>
+                <a href="">view more</a>
+                <a href="" class="btn projects-blockchainz-design-btn">explore</a>
+            </div>
+        </div>
+
+        <!-- Web3.fm  -->
+        <div class="slide web3-slide projects-container">
+            <div class="figure-mock web3-mock">
+                <div class="web3-image1"></div>
+                <div class="web3-image2"></div>
+            </div>
+            <div>
+                <h3>Web3.fm</h3>
+                <p>A podcast dedicated to sharing knowledge and insights about building products that work with real people in the real world - on blockchain.</p>
+                <a href="">view more</a>
+                <a href="" class="btn web3-btn">follow</a>
+            </div>
         </div>
     </div>
-
-    <!-- Bizzllet -->
-    <div class="slide bizzllet-slide">
-        <div>
-            <h3>Bizzllet</h3>
-            <p>Bizzllet is imagined as a platform for business users that will bring new opportunities to seamlessly store, manage, integrate and distribute a company's cryptocurrency portfolio across multiple wallets with different integrations to meet the business needs. All that packed into a user-friendly interface for everyone coming from the current Web2 world to be able to easily enter the new era of payments and doing business on the blockchain.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-bizzllet-btn">check prototype</a>
-        </div>
-    </div>
-
-    <!-- Substrate Blockchain Starter Kit -->
-    <div class="slide sbsk-slide">
-        <div>
-            <h3>Substrate Blockchain <br>Starter Kit</h3>
-            <p>It is a tool that brings the simplicity of creating a blockchain to another level! By drag and dropping pallets to a codebase, anyone can have a customized blockchain deployed in less than a minute.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-sbsk-btn">build your blockchain</a>
-        </div>
-    </div>
-
-    <!-- Stealth DEX PoC L2  -->
-    <div class="slide sdpl-slide">
-        <div>
-            <h3>Stealth DEX PoC L2</h3>
-            <p>Stealth DEX is a L2 orderbook based, spot trading exchange protocol built on top of Ethereum and Loopring.DEX uses zk-proofs to validate all trades on the Ethereum mainnet, thus effectively deriving security from it. It uses a "Volition" approach, i.e. hybrid on-chain/off-chain data storage. The flexibility of the "Volition" approach still allows users to maintain the highest level of security by allowing them to store the data on the Ethereum mainnet (just like they do with "Rollups") or locally, i.e. using their own storage.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-sdpl-btn">work with us</a>
-        </div>
-    </div>
-
-    <!-- Whollet -->
-    <div class="slide whollet-slide">
-        <div>
-            <h3>Whollet</h3>
-            <p>Whollet is a comprehensive crypto wallet UI kit we created by gathering the best practices (some of which we developed) for everyone to use and customize freely. You may recognize many UI elements, as many projects such as Coinbase, Argent, Celsius, and others are using similar information architecture and interaction patterns.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-whollet-btn">download</a>
-        </div>
-    </div>
-
-    <!-- Smart Contract Vulnerabilities Database -->
-    <div class="slide scvd-slide">
-        <div>
-            <h3>Smart Contract <br>Vulnerabilities <br>Database</h3>
-            <p>Whollet is a comprehensive crypto wallet UI kit we created by gathering the best practices (some of which we developed) for everyone to use and customize freely. You may recognize many UI elements, as many projects such as Coinbase, Argent, Celsius, and others are using similar information architecture and interaction patterns.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-scvd-btn">download</a>
-        </div>
-    </div>
-
-    <!-- Crypto Gift Cards -->
-    <div class="slide cgc-slide">
-        <div>
-            <h3>Crypto Gift Cards</h3>
-            <p>A tool for the creation of gift cards topped up with cryptocurrency. Simple way to introduce someone to cryptocurrencies.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-cgc-btn">create a card</a>
-        </div>
-    </div>
-
-    <!-- Crypto Trophies -->
-    <div class="slide crypto-trophies-slide">
-        <div>
-            <h3>Crypto Trophies</h3>
-            <p>Crypto trophies is an employee recognition tool offering unique badges (NFTs) for organization or company promotion/employee growth + scoreboard within the organization (passed trial, got promotion, anniversary, peers recognition, etc.)</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-crypto-trophies-btn">give some recognition</a>
-        </div>
-    </div>
-
-    <!-- Liquid Crystal  -->
-    <div class="slide liquid-crystal-slide">
-        <div>
-            <h3>Liquid Crystal</h3>
-            <p>Liquid Crystal is a blockchain created with an idea to avoid the current bottleneck where all transactions are targeting shared ledger on the blockchain, by enabling communication on a smaller scale. Imagine different blockchains composed into a tree hierarchy working and communicating together as one. This model is enabling faster transactions together with keeping gas fees very low. Too good to be true?</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-liquid-crystal-btn">join the crew</a>
-        </div>
-    </div>
-
-    <!-- Our network -->
-    <div class="slide our-network-slide">
-        <div>
-            <h3>Our network</h3>
-            <p>A community-driven platform for cryptanalysis. With Our Network, analysts from any discipline can contribute concise insights to the broader community while gaining valuable feedback in the form of voting and commentary. And end-users can receive a curated and personalized crypto analytics experience.Anyone should be able to take the data sources available today and derive unique, concise insights for users, investors, and teams to learn from; effectively creating a leaderboard with the best data-driven content curated by the community members.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-our-network-btn">join now</a>
-        </div>
-    </div>
-
-    <!-- Decentralized Business Model Canvas -->
-    <div class="slide dbmc-slide">
-        <div>
-            <h3>Decentralized Business <br>Model Canvas</h3>
-            <p>New approach to the classic business model canvas, adapted for validation of decentralized business models.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-dbmc-btn">join the crew</a>
-        </div>
-    </div>
-
-    <!-- Token Modelling Canvas -->
-    <div class="slide tmc-slide">
-        <div>
-            <h3>Token Modelling <br>Canvas</h3>
-            <p>New take on a framework for designing balanced and sustainable tokenomics models. It's been created as an additional tool to be used with DBMC, but it can be used entirely separately.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-tmc-btn">download</a>
-        </div>
-    </div>
-
-    <!-- Blockchainz Design -->
-    <div class="slide blockchainz-design-slide">
-        <div>
-            <h3>Blockchainz Design</h3>
-            <p>A place for designers in the blockchain world, showcasing the best projects, articles, resources and ideas.</p>
-            <a href="">view more</a>
-            <a href="" class="btn projects-blockchainz-design-btn">explore</a>
-        </div>
-    </div>
-
-    <!-- Web3.fm  -->
-    <div class="slide web3-slide">
-        <div>
-            <h3>Web3.fm</h3>
-            <p>A podcast dedicated to sharing knowledge and insights about building products that work with real people in the real world - on blockchain.</p>
-            <a href="">view more</a>
-            <a href="" class="btn web3-btn">follow</a>
-        </div>
-    </div>
-<?php get_footer(); ?>
+<?php get_footer('privacy-policy'); ?>
