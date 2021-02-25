7a0c5dd230a9d7549e03dc6ed4c2f482a7ab1fb0
satrain <mladen.trickovic5@gmail.com>
2021-02-25 11:52:27 +0100

    404 page add

--- 404.php
+++ 404.php
@@ -3,6 +3,7 @@
 ?>
 
 <div class="not-found-container">
+    
     <h1>Error 404</h1>
     <h2>Damn, you sir are pushing things far! 
         <br>
@@ -10,7 +11,7 @@
     <p>But guess what you didn’t find?</p>
     <p>That page you were looking for. Smarty-pants!</p>
     <a href="<?php _e(get_site_url()); ?>" class="btn">go back to home</a>
-    <!--<div class="not-found-image"></div>-->
+    <div class="not-found-image"></div>
 </div>
 
 <?php get_footer('privacy-policy'); ?>
